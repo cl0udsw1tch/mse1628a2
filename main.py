b989c1e8e4b011528ceb7853a3deb6388933cb14
cl0udsw1tch <nurein.umeya@mail.utoronto.ca>
2024-11-01 18:44:04 -0400

    Initiating spin!

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 import pandas as pd
-import pickle, os
+import pickle, os, sys
 from concurrent.futures import ProcessPoolExecutor
 
 
@@ -48,59 +48,84 @@ density_featurizer = DensityFeatures()
 # chemical_featurizer = ChemicalSRO(nn=6).fit(data["structure"])
 
 
+structural_featurizer = MultipleFeaturizer([
+    density_featurizer,
+    # struct_het_featurizer,
+])
+
 compositional_featurizer = MultipleFeaturizer([
     stoich_featurizer,
     ion_prop_featurizer,
     # e_affinity_featurizer,
     # atomic_orb_featurizer,
     valence_orb_featurizer,
-    # element_featurizer,
-])
-structural_featurizer = MultipleFeaturizer([
-    #density_featurizer,
-    struct_het_featurizer,
+    element_featurizer,
 ])
 
 def featurize_chunk(chunk, i):
     # composition = pickle.loads(composition_bytes)
 
-    structural_featurizer.featurize_dataframe(chunk, col_id="structure", ignore_errors=True, inplace=True)
-    compositional_featurizer.featurize_dataframe(chunk, col_id="composition", ignore_errors=True, inplace=True)
+    structural_features = structural_featurizer.featurize_dataframe(chunk, col_id="structure", ignore_errors=True, inplace=False)
+    compositional_features = compositional_featurizer.featurize_dataframe(chunk, col_id="composition", ignore_errors=True, inplace=False)
     
     # Combine features
-    print(f"Processed chunk: {i}")
-    return chunk
+    return [structural_features, compositional_features]
 
 
 
 def main():
+    n_workers =int(sys.argv[1])
+    
     data_path = "matbench_mp_gap_raw.data"
-    featurized_path = "matbench_mp_gap_featurized2.data"
-    # with open(data_path, "rb") as f:
-    #     data = pickle.load(f)
-    with open("matbench_mp_gap_featurized.data", "rb") as f:
+    featurized_path = "matbench_mp_gap_featurized.data"
+    with open(data_path, "rb") as f:
         data = pickle.load(f)
-        
-    # data['composition'] = data['structure'].apply(lambda struct: struct.composition)
-    # compositional_bytes = data['composition'].apply(lambda x: pickle.dumps(x))
-    
-    non_magpie = data.copy().iloc[:, :7]
 
-    
-    chunk_size = 128
-    chunks = [non_magpie[i:i + chunk_size] for i in range(0, 2*chunk_size, chunk_size)]
-
-    with ProcessPoolExecutor(max(1, os.cpu_count() - 2)) as executor:
-        features = pd.concat(list(executor.map(featurize_chunk, chunks, [0,1])), axis=0)
         
-    df_features = pd.DataFrame(features, 
-                               columns= structural_featurizer.feature_labels() + 
-                                        compositional_featurizer.feature_labels())
-    df_featurized = pd.concat([non_magpie.iloc[:2 * chunk_size, :7], df_features, data.iloc[:2*chunk_size, 7:]], axis=1)
+    data['composition'] = data['structure'].apply(lambda struct: struct.composition)
+    
+    chunk_size = int(sys.argv[2])
+    print(f"Chunk size: {chunk_size}")
+    chunks = [data[i:i + chunk_size] for i in range(0, len(data), chunk_size)]
+
+    folder_path = "features"
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
+
+  
+    with ProcessPoolExecutor(max_workers= n_workers) as executor:
+        features = list(executor.map(featurize_chunk, chunks, range(n_workers)))
+    
+
+    structural_features_list, compositional_features_list = zip(*features)
+    df_structural_features = pd.concat(structural_features_list, axis=0)
+    df_compositional_features = pd.concat(compositional_features_list, axis=0)
+
+    # Combine features
+    df_featurized = pd.concat([data, df_structural_features, df_compositional_features], axis=1)
 
     with open(featurized_path, "wb") as f:
         pickle.dump(df_featurized, f)
+
+    
+
+  
+    # with open(featurized_path, "wb") as f:
+    #     pickle.dump(df_featurized, f)
         
         
 if __name__ == "__main__":
     main()
+
+    # data_path = "matbench_mp_gap_raw.data"
+    # featurized_path = "matbench_mp_gap_featurized.data"
+    # with open(data_path, "rb") as f:
+    #     data = pickle.load(f)
+
+    # structural_features = structural_featurizer.featurize_dataframe(data, col_id="structure", ignore_errors=True, inplace=False)
+    # compositional_features = compositional_featurizer.featurize_dataframe(data, col_id="composition", ignore_errors=True, inplace=False)
+    
+    # # Combine features
+    # df_featurized = pd.concat([data, structural_features, compositional_features], axis=1)
+    # with open(featurized_path, "wb") as f:
+    #     pickle.dump(df_featurized, f)
